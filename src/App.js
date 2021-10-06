5b62a14656e8852375f2b33fa266772888e344cf
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-06 16:28:33 -0500

    First edition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
+  const [space, setSpace] = useState([]);
+
+  useEffect(() => {
+    const nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=3G17yns5gZSwWbFKInHZRM0WDVVXIa8pkJcumR2o';
+    axios.get(`${nasaURL}`)
+    .then(res => {
+      setSpace(res.data);
+      console.log(res.data);
+    }).catch(err => {
+      console.error(err);
+    })
+  }, [])
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        Daily Space Photo! <br/>
+        {space.date}
       </p>
+      <p>
+        {space.title} <br/>
+        &copy; {space.copyright}
+      </p>
+      <div className="img wrapper">
+        <img src={space.url} alt="space"></img>
+      </div>
+      <div className='details'>
+        <p>
+          {space.explanation}
+        </p>
+      </div>
+
     </div>
   );
 }
